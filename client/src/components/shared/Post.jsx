c9697e554c97cfae191672666e26ca8b9fdfb914
yasin al hasan <ya705668@gmail.com>
2025-06-22 16:06:03 +0600

    add like and dislike setup

--- client/src/components/shared/Post.jsx
+++ client/src/components/shared/Post.jsx
@@ -6,16 +6,22 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import CommentDialog from "./CommentDialog";
 // import { Input } from "../ui/input";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 import axios from "axios";
+import { setPosts } from "@/redux/postSlice";
 
 const Post = ({ post }) => {
   const [text, setText] = useState("");
   const [open, setOpen] = useState(false);
-  const [showFullCaption, setShowFullCaption] = useState(false);
-  // const [deleteOpen, setDeleteOpen] = useState(false);
   const { user } = useSelector((store) => store.auth);
+  const { posts } = useSelector((store) => store.post);
+  const [liked, setLiked] = useState(post.likes?.includes(user?._id) || false);
+  const [showFullCaption, setShowFullCaption] = useState(false);
+  const [postLike, setPostLike] = useState(post.likes.length);
+
+  const dispatch = useDispatch();
+  // const [deleteOpen, setDeleteOpen] = useState(false);
   const changeEventHandler = (e) => {
     const inputText = e.target.value;
     // Limit the input text to 100 characters
@@ -26,6 +32,66 @@ const Post = ({ post }) => {
     }
   };
 
+  // Function to handle the like button click
+  const likeOrDislikeHandler = async () => {
+    try {
+      const action = liked ? "dislike" : "like";
+      const res = await axios.get(
+        `http://localhost:5000/api/v1/post/${post._id}/${action}`,
+        {
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        const updatedLikes = liked ? postLike - 1 : postLike + 1;
+        setPostLike(updatedLikes);
+        setLiked(!liked);
+
+        // Update the posts in the Redux store
+        const updatedPostData = posts.map((p) =>
+          p._id === post._id
+            ? {
+                ...p,
+                likes: liked
+                  ? p.likes.filter((id) => id !== user._id)
+                  : [...p.likes, user._id],
+              }
+            : p
+        );
+        dispatch(setPosts(updatedPostData));
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.error("Error liking or disliking post:", error);
+      toast.error(
+        error.response?.data?.message ||
+          "An error occurred while liking or disliking the post."
+      );
+    }
+  };
+
+  // Function to handle the comment button click
+  const commentHandler = () => {
+    try {
+      const res = axios.post(`http://localhost:5000/api/v1/post/${post._id}/comment`, {text}, {
+        headers:{
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      })
+
+    } catch (error) {
+      console.error("Error handling comment:", error);
+      toast.error(
+        error.response?.data?.message ||
+          "An error occurred while handling the comment."
+      );
+      
+    }
+  }
+
+  // delete post handler
+
   const deletePostHandler = async () => {
     try {
       const res = await axios.delete(
@@ -35,6 +101,10 @@ const Post = ({ post }) => {
         }
       );
       if (res.data.success) {
+        const updatedPosts = posts.filter(
+          (postItem) => postItem._id !== post?._id
+        ); // Assuming the response contains the updated posts
+        dispatch(setPosts(updatedPosts));
         toast.success(res.data.message);
         setOpen(false);
         console.log("Post deleted successfully:", res.data.post);
@@ -105,16 +175,10 @@ const Post = ({ post }) => {
 
                     <Button
                       onClick={deletePostHandler}
-                      variant={"outline"}
+                      variant={"destructive"}
                       className={"cursor-pointer w-full my-2"}
                     >
                       Confirm
-                    </Button>
-                    <Button
-                      variant={"destructive"}
-                      className={"cursor-pointer w-full my-2 "}
-                    >
-                      Cancel
                     </Button>
                   </DialogContent>
                 </Dialog>
@@ -131,10 +195,20 @@ const Post = ({ post }) => {
 
       <div className="flex items-center justify-between my-2">
         <div className="flex items-center gap-3">
-          <FaRegHeart
-            size={"22px"}
-            className={"cursor-pointer hover:text-gray-600"}
-          />
+          {liked ? (
+            <FaHeart
+              onClick={likeOrDislikeHandler}
+              size={"24"}
+              className="cursor-pointer text-[#FBCF28]"
+            />
+          ) : (
+            <FaRegHeart
+              onClick={likeOrDislikeHandler}
+              size={"22px"}
+              className={"cursor-pointer text-gray-600 hover:text-[#FBCF28]"}
+            />
+          )}
+
           <MessageCircle
             onClick={() => setOpen(true)}
             className={"cursor-pointer hover:text-gray-600"}
@@ -143,9 +217,7 @@ const Post = ({ post }) => {
         </div>
         <Bookmark className={"cursor-pointer hover:text-gray-600"} />
       </div>
-      <span className="font-medium block mb-2">
-        {post.likes?.length} Likes{" "}
-      </span>
+      <span className="font-medium block mb-2">{postLike} Likes </span>
       <p>
         <span className="font-medium mr-2">@{post.author?.username}:</span>
         {displayedCaption}
